86467163a73c68a25470780df6eac662aa638470
kimdoyoung <57849386+coby5502@users.noreply.github.com>
2022-05-22 17:23:42 +0900

    Initial Commit

---
+++ SwiftfulThinkingBootcamp/SwiftfulThinkingBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulThinkingBootcampApp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by COBY_PRO on 2022/05/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulThinkingBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
