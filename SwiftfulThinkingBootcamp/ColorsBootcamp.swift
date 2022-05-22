7a749e0f2ca24ecb2afd2ce9e07f0665903bfa5d
Doyoung <coby5502@gmail.com>
2022-05-22 21:17:48 +0900

    Bootcamp #4
    Color, UIColor, Color Literals, and Hex Colors in SwiftUI

---
+++ SwiftfulThinkingBootcamp/ColorsBootcamp.swift
@@ -0,0 +1,30 @@
+//
+//  ColorsBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by COBY_PRO on 2022/05/22.
+//
+
+import SwiftUI
+
+struct ColorsBootcamp: View {
+    var body: some View {
+        RoundedRectangle(cornerRadius: 25.0)
+            .fill(
+                //Color.primary
+                //Color(#colorLiteral(red: 0.4352941176, green: 0.5607843137, blue: 0.9176470588, alpha: 1))
+                //Color(UIColor.secondarySystemBackground)
+                Color("CustomColor")
+            )
+            .frame(width: 300, height: 200)
+            //.shadow(radius: 10)
+            .shadow(color: Color("CustomColor").opacity(0.3), radius: 10, x: -20, y: -20)
+    }
+}
+
+struct ColorsBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        ColorsBootcamp()
+            
+    }
+}
