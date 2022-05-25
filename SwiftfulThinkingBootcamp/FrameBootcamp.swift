545556ecaac7529db52c3ebef8865aa0ec949e51
Doyoung <coby5502@gmail.com>
2022-05-26 00:10:45 +0900

    Bootcamp #8
    Frames and Alignments in SwiftUI

---
+++ SwiftfulThinkingBootcamp/FrameBootcamp.swift
@@ -0,0 +1,35 @@
+//
+//  FrameBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by COBY_PRO on 2022/05/25.
+//
+
+import SwiftUI
+
+struct FrameBootcamp: View {
+    var body: some View {
+        Text("Hello, World!")
+            //.background(Color.green)
+            //.frame(width: 300, height: 300, alignment: .leading)
+            //.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+            //.background(Color.red)
+            .background(Color.red)
+            .frame(height: 100, alignment: .top)
+            .background(Color.orange)
+            .frame(width: 150)
+            .background(Color.purple)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .background(Color.pink)
+            .frame(height: 400)
+            .background(Color.green)
+            .frame(maxHeight: .infinity, alignment: .top)
+            .background(Color.yellow)
+    }
+}
+
+struct FrameBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        FrameBootcamp()
+    }
+}
