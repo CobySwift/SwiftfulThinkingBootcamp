360ed6ae154ffef78d7a67fec02681fa68ad3f06
Doyoung <coby5502@gmail.com>
2022-06-09 16:17:20 +0900

    Bootcamp #14
    How to use ForEach loops in SwiftUI

---
+++ SwiftfulThinkingBootcamp/ForEachBootcamp.swift
@@ -0,0 +1,33 @@
+//
+//  ForEachBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by COBY_PRO on 2022/06/09.
+//
+
+import SwiftUI
+
+struct ForEachBootcamp: View {
+    
+    let data: [String] = ["Hi", "Hello", "Hey everyone"]
+    let myString: String = "Hello"
+    
+    var body: some View {
+        VStack {
+            ForEach(data.indices) { index in
+                Text("\(data[index]): \(index)")
+            }
+            ForEach(0..<100) { index in
+                Circle()
+                    .frame(height: 50)
+            }
+        }
+    }
+}
+
+struct ForEachBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        ForEachBootcamp()
+    }
+}
+ 
